c4447426ac17b1b26be2fdcbf5bfac2e101612ca
ThiagoPerez12 <thperez@frba.utn.edu.ar>
2025-05-07 12:32:16 -0300

    Parte 1

--- src/Library.hs
+++ src/Library.hs
@@ -1,6 +1,5 @@
 module Library where
 import PdePreludat
-
 data Ingrediente =
     Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
     deriving (Eq, Show)
@@ -18,3 +17,28 @@ data Hamburguesa = Hamburguesa {
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
 
+cuartoDeLibra :: Hamburguesa
+cuartoDeLibra = Hamburguesa 20 [Pan, Carne, Cheddar, Pan]
+
+precioFinal :: Hamburguesa -> Number
+precioFinal (Hamburguesa preciobase ingredientes) = preciobase + sumatoriaPrecio ingredientes
+
+sumatoriaPrecio :: [Ingrediente] -> Number
+sumatoriaPrecio = sum . map precioIngrediente
+
+agrandar :: Hamburguesa -> Hamburguesa
+agrandar unaHamburguesa
+    |tieneIngrediente Carne unaHamburguesa = agregarIngrediente Carne unaHamburguesa 
+    |tieneIngrediente Pollo unaHamburguesa = agregarIngrediente Pollo unaHamburguesa 
+    
+tieneIngrediente :: Ingrediente -> Hamburguesa -> Bool
+tieneIngrediente unIngrediente unaHamburguesa = any (==unIngrediente) (ingredientes unaHamburguesa)
+
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
+agregarIngrediente unIngrediente unaHamburguesa = unaHamburguesa {ingredientes = ingredientes unaHamburguesa ++ [unIngrediente]} 
+
+descuento :: Number -> Hamburguesa -> Hamburguesa
+descuento porcentajeDescuento unaHamburguesa = unaHamburguesa {precioBase = precioBase unaHamburguesa * (1 - (porcentajeDescuento/100))}
+
+pdepBurger :: Hamburguesa
+pdepBurger = (descuento 20 . agregarIngrediente Cheddar . agregarIngrediente Panceta . agrandar . agrandar) cuartoDeLibra
